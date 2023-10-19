7ac62b8356ebbf4cad4c5347bcc3d852bf359069
nicolasgorzio <gorzionicolas@gmail.com>
2023-10-18 22:18:32 -0300

    implementacion view router y nav bar funcional :)

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TiendaView from '../views/TiendaView.vue'
 import LoginView from '../views/LoginView.vue'
-
+import ConocenosView from '../views/ConocenosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +29,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/conocenos',
+      name: 'conocenos',
+      component: ConocenosView
     }
 
   ]
